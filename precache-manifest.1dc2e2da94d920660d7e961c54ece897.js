46fcabbab24e02c7d8cde47c475c994084e3c774
Jussi <misterkarppinen@gmail.com>
2019-02-13 14:10:45 +0200

    Updates

---
+++ precache-manifest.1dc2e2da94d920660d7e961c54ece897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7aa9d0ff8ebfdd82bacf984870bcb257",
+    "url": "/static/media/loading.7aa9d0ff.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "69969f41b95f26fa93d7",
+    "url": "/static/js/main.69969f41.chunk.js"
+  },
+  {
+    "revision": "a3876aefdcc085937616",
+    "url": "/static/js/2.a3876aef.chunk.js"
+  },
+  {
+    "revision": "69969f41b95f26fa93d7",
+    "url": "/static/css/main.c1b89203.chunk.css"
+  },
+  {
+    "revision": "7c67d95e4a4c709b28e38a127bdc4392",
+    "url": "/index.html"
+  }
+];
