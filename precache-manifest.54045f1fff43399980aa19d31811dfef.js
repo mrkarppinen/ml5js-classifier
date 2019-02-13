490b32a73f83c309f457b556a19e588d4aaa85b7
Jussi <misterkarppinen@gmail.com>
2019-02-13 13:49:30 +0200

    Updates

---
+++ precache-manifest.54045f1fff43399980aa19d31811dfef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7aa9d0ff8ebfdd82bacf984870bcb257",
+    "url": "/ml5js-classifier/static/media/loading.7aa9d0ff.svg"
+  },
+  {
+    "revision": "16ca586d27809185f1d1",
+    "url": "/ml5js-classifier/static/js/runtime~main.16ca586d.js"
+  },
+  {
+    "revision": "b5a824a6e11f02e13de0",
+    "url": "/ml5js-classifier/static/js/main.b5a824a6.chunk.js"
+  },
+  {
+    "revision": "1594e938cf6a3ee7bd48",
+    "url": "/ml5js-classifier/static/js/2.1594e938.chunk.js"
+  },
+  {
+    "revision": "b5a824a6e11f02e13de0",
+    "url": "/ml5js-classifier/static/css/main.c1b89203.chunk.css"
+  },
+  {
+    "revision": "0476df31552381608989054f04e8c940",
+    "url": "/ml5js-classifier/index.html"
+  }
+];
